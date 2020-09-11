57a86bcbb71fca7867878da5d22d6e95e7361c4b
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-11 21:31:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-#Write a method called reverse_each_word that takes in a string argument of a 
+#Write a method called reverse_each_word that takes in a string argument of a
 #sentence and returns that same sentence with each word reversed in place.
 def reverse_each_word(string)
 
