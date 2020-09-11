f924e1320c678806f6922364c8a647c77dae2c10
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-09-11 21:43:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 #Write a method called reverse_each_word that takes in a string argument of a
 #sentence and returns that same sentence with each word reversed in place.
 def reverse_each_word(sentence)
-  sentence.split.collect do |word|
-    word.reverse.join(" ")
+  sentence.reverse.split.reverse.join(" ")
   end
 end
